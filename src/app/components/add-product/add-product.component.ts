b6e92793932b5e82cbabcb4d2f6268b8c3509d5e
jesustt <minicascos@gmail.com>
2020-11-16 02:33:13 +0100

    invoice service created

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -21,13 +21,13 @@ export class AddProductComponent implements OnInit {
   }
 
   public saveProduct():void{
-    /*this.service.createProduct(this.product).subscribe(product => {
-     
-      
-    })*/
+    this.service.createProduct(this.product).subscribe(product => {
     console.log(this.product);
     alert(`Product Created:${this.product.productName}`);
     this.router.navigate(['/products']);
+      
+    });
+    
   }
 
 }
