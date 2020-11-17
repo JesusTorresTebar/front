b39f129ab37d9222484f1b03f1726c23595cae3d
jesustt <minicascos@gmail.com>
2020-11-17 05:46:12 +0100

    Customer,Provider and Product Edit and Delete  earlyaccess

--- src/app/components/individual-product/individual-product.component.ts
+++ src/app/components/individual-product/individual-product.component.ts
@@ -9,10 +9,12 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./individual-product.component.css']
 })
 export class IndividualProductComponent implements OnInit {
+  titulo: string = 'Product Details';
   product:Product;
+  newProduct:Product;
   sub:any;
   id:string;
-  constructor(private service:ProductService,private route:ActivatedRoute) {
+  constructor(private service:ProductService,private route:ActivatedRoute,private router:Router) {
   
    }
 
@@ -22,9 +24,29 @@ export class IndividualProductComponent implements OnInit {
 
       this.service.finddProductById(this.id).subscribe(product =>{
         this.product=product;
+        this.newProduct=product;
       });
     });
     
+  }
+
+  public updateProduct():void{
+    this.service.createProduct(this.newProduct).subscribe(product =>{
+    console.log(this.newProduct);
+    alert(`Product Updated.ID:${this.newProduct.id}`);
+    this.router.navigate(['/products']);
+    });
+    
+  }
+
+  public deleteProduct():void{
+   this.service.deleteProduct(this.product.id).subscribe();
+   alert(`Product Deleted.ID:${this.newProduct.id}`);
+   this.router.navigate(['/products']);
+  }
+
+  public backToList():void{
+    this.router.navigate(['/products']);
   }
 
 }
--- src/app/components/individual-provider/individual-provider.component.ts
+++ src/app/components/individual-provider/individual-provider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Provider } from 'src/app/models/provider';
 import { InvoiceService } from 'src/app/services/invoice.service';
 
@@ -9,10 +9,12 @@ import { InvoiceService } from 'src/app/services/invoice.service';
   styleUrls: ['./individual-provider.component.css']
 })
 export class IndividualProviderComponent implements OnInit {
+  titulo: string = 'Providers Details';
   provider:Provider;
+  newProvider:Provider;
   sub:any;
   id:string;
-  constructor(private service:InvoiceService,private route:ActivatedRoute) { }
+  constructor(private service:InvoiceService,private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit(): void {
     this.sub=this.route.params.subscribe(params =>{
@@ -20,9 +22,29 @@ export class IndividualProviderComponent implements OnInit {
 
       this.service.findProviderById(this.id).subscribe(provider =>{
         this.provider=provider;
+        this.newProvider=provider;
       });
     });
     
+  }
+
+  public updateProvider():void{
+    this.service.createProvider(this.newProvider).subscribe(newProvider =>{
+    console.log(this.newProvider);
+    alert(`Provider Updated.DNI:${this.newProvider.id}`);
+    this.router.navigate(['/providers']);
+    });
+    
+  }
+
+  public deleteProvider():void{
+   this.service.deleteProvider(this.provider.id).subscribe();
+   alert(`Provider Deleted.DNI:${this.newProvider.id}`);
+   this.router.navigate(['/providers']);
+  }
+
+  public backToList():void{
+    this.router.navigate(['/providers']);
   }
 
 }
