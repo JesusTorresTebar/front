1eeeeb4e2a8024f051b1be022cc8060a0c1bbf7e
jesustt <minicascos@gmail.com>
2020-11-18 04:02:23 +0100

    Customer,Provider and Product Edit and Delete  earlyaccess

--- src/app/components/add-customer-invoice/add-customer-invoice.component.ts
+++ src/app/components/add-customer-invoice/add-customer-invoice.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/models/customer';
+import { Product } from 'src/app/models/product';
+import { InvoiceService } from 'src/app/services/invoice.service';
+import { ProductService } from 'src/app/services/product.service';
+import { map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-customer-invoice',
@@ -6,12 +14,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-customer-invoice.component.css']
 })
 export class AddCustomerInvoiceComponent implements OnInit {
+  selectCustomer: boolean=true;
+  showCustomer:boolean=false;
+  customer: Customer;
+  customerList: Customer[];
+  productList: Product[];
 
-  constructor() { }
+  /*Codigo dropList*/
+  myControl = new FormControl();
+  options: string[] = null;
+  filteredOptions: Observable<string[]>;
+
+
+  constructor(private serviceInvoice:InvoiceService, private route:ActivatedRoute, private router:Router, private serviceProduct:ProductService) { }
 
   ngOnInit(): void {
+
+    this.serviceInvoice.findAllCustomer().subscribe(customerList =>{
+      this.customerList=customerList;
+      
+    });
+
+    for(let i=0;i<this.customerList.length;i++){
+      this.options[i]=this.customerList[i].name;
+    }
+    this.serviceProduct.findAllProduct().subscribe(productList =>{
+      this.productList=productList;
+    });
+
+
+    /*Codigo dropList*/
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+
+/*Codigo dropList*/
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+
+  public backToList():void{
+    this.router.navigate(['/home']);
+  }
   }
 
 
 
-}
+
+
+
+
+
