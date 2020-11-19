22215a35303c675c82c4175b3eda1db671bc1838
jesustt <minicascos@gmail.com>
2020-11-19 05:11:46 +0100

    Invoice Creation early access

--- src/app/components/add-customer-invoice/add-customer-invoice.component.ts
+++ src/app/components/add-customer-invoice/add-customer-invoice.component.ts
@@ -6,7 +6,7 @@ import { Customer } from 'src/app/models/customer';
 import { Product } from 'src/app/models/product';
 import { InvoiceService } from 'src/app/services/invoice.service';
 import { ProductService } from 'src/app/services/product.service';
-import { map, startWith} from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 import { InvoiceDetail } from 'src/app/models/invoiceDetail';
 import { CustomerInvoice } from 'src/app/models/customerInvoice';
 
@@ -19,55 +19,50 @@ import { CustomerInvoice } from 'src/app/models/customerInvoice';
   styleUrls: ['./add-customer-invoice.component.css']
 })
 export class AddCustomerInvoiceComponent implements OnInit {
-  selectCustomer: boolean=true;
-  showCustomer:boolean=false;
+  selectCustomer: boolean = true;
+  showCustomer: boolean = false;
 
   customer: Customer;
-  customerList: Customer[]=[];
+  customerList: Customer[] = [];
 
-  invoiceCreated:boolean=false;
-  customerInvoice:CustomerInvoice=new CustomerInvoice();
-  newCustomerInvoice:CustomerInvoice=new CustomerInvoice();
+  invoiceCreated: boolean = false;
+  customerInvoice: CustomerInvoice = new CustomerInvoice();
+  newCustomerInvoice: CustomerInvoice = new CustomerInvoice();
 
-  emptyDetail:InvoiceDetail=new InvoiceDetail();
+  emptyDetail: InvoiceDetail = new InvoiceDetail();
 
-  productList: Product[]=[];
+  productList: Product[] = [];
 
-  detailList: InvoiceDetail[]=[];
-  productDetailList:Product[]=[];
+  detailList: InvoiceDetail[] = [];
+  productDetailList: Product[] = [];
   /*
   productQuantityList:number[]=[];
   productTotalList:number[]=[];*/
 
-  selectedCustomerName:string;
+  selectedCustomerName: string;
   /*Codigo dropList*/
   myControl = new FormControl();
   options: string[] = [];
   filteredOptions: Observable<string[]>;
 
 
-  constructor(private serviceInvoice:InvoiceService, private route:ActivatedRoute, private router:Router, private serviceProduct:ProductService) { }
+  constructor(private serviceInvoice: InvoiceService, private route: ActivatedRoute, private router: Router, private serviceProduct: ProductService) { }
 
   ngOnInit(): void {
 
-    this.serviceInvoice.findAllCustomer().subscribe(customerList =>{
-      this.customerList=customerList;
+    this.serviceInvoice.findAllCustomer().subscribe(customerList => {
+      this.customerList = customerList;
       console.log("tamaño del array" + customerList.length);
-      for(let i=0;i<customerList.length;i++){
-        this.options.push(customerList[i].name);                   
+      for (let i = 0; i < customerList.length; i++) {
+        this.options.push(customerList[i].name);
       }
 
-      /*detalle vacio*/
 
-      this.emptyDetail.productId="0005";
-      this.emptyDetail.quantity=1;
-      this.emptyDetail.total=10
-      this.emptyDetail.unitPrice=10;
     });
 
 
-    this.serviceProduct.findAllProduct().subscribe(productList =>{
-      this.productList=productList;
+    this.serviceProduct.findAllProduct().subscribe(productList => {
+      this.productList = productList;
     });
 
 
@@ -80,7 +75,7 @@ export class AddCustomerInvoiceComponent implements OnInit {
   }
 
 
-/*Codigo dropList*/
+  /*Codigo dropList*/
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
@@ -88,40 +83,47 @@ export class AddCustomerInvoiceComponent implements OnInit {
   }
 
 
-  public backToList():void{
+  public backToList(): void {
     this.router.navigate(['/home']);
   }
 
 
-  onSelectCustomer(option:string):void{
-    this.selectedCustomerName=option;
-    console.log("OPTION: "+option);
-    for(let i=0;i<this.customerList.length;i++){
-      console.log("OPTION: "+option + " LIST: "+this.customerList[i]);
-      if(this.customerList[i].name.toLowerCase()==option.toLowerCase()){
-        
-        this.customer=this.customerList[i];
+  onSelectCustomer(option: string): void {
+    this.selectedCustomerName = option;
+    console.log("OPTION: " + option);
+    for (let i = 0; i < this.customerList.length; i++) {
+      console.log("OPTION: " + option + " LIST: " + this.customerList[i]);
+      if (this.customerList[i].name.toLowerCase() == option.toLowerCase()) {
+
+        this.customer = this.customerList[i];
       }
     }
-    this.showCustomer=true;
-    this.selectCustomer=false;
+    this.showCustomer = true;
+    this.selectCustomer = false;
 
     /*Creación de la factura.*/
-    this.customerInvoice.customerId=this.customer.id;
+    this.customerInvoice.customerId = this.customer.id;
 
     /*Obtención fecha*/
     let date = new Date();
-    this.customerInvoice.invoiceDate=date;
+    this.customerInvoice.invoiceDate = date;
 
-    this.customerInvoice=this.updateInvoicePrice(10,this.customerInvoice);
-    this.customerInvoice.invoiceDetail=[];
-    this.customerInvoice.invoiceDetail.push(this.emptyDetail);
+    this.customerInvoice = this.updateInvoicePrice(10, this.customerInvoice);
+
+    /*detalle vacio*/
+
+    this.emptyDetail.productId = "0005";
+    this.emptyDetail.quantity = 1;
+    this.emptyDetail.total = 10
+    this.emptyDetail.unitPrice = 10;
+    this.customerInvoice.invoiceDetail = [this.emptyDetail];
+
 
     /*Creamos la factura y recibimos la nueva*/
-    this.serviceInvoice.createCustomerInvoice(this.customerInvoice).subscribe(customerInvoice=>{
-      this.newCustomerInvoice=customerInvoice;
-      this.invoiceCreated=true;
-      console.log("la factura es:"+this.newCustomerInvoice.id);
+    this.serviceInvoice.createCustomerInvoice(this.customerInvoice).subscribe(customerInvoice => {
+      this.newCustomerInvoice = customerInvoice;
+      this.invoiceCreated = true;
+      console.log("la factura es:" + this.newCustomerInvoice.id);
 
     });
   }
@@ -129,13 +131,13 @@ export class AddCustomerInvoiceComponent implements OnInit {
 
 
 
-  updateInvoicePrice(subtotal:number,customerInvoice:CustomerInvoice):CustomerInvoice{
-    customerInvoice.subtotal=subtotal;
-    customerInvoice.vatPercentage=0.21;
-    customerInvoice.vat=customerInvoice.vatPercentage*customerInvoice.subtotal;
-    customerInvoice.total=customerInvoice.subtotal+customerInvoice.vat;
-    
-    
+  updateInvoicePrice(subtotal: number, customerInvoice: CustomerInvoice): CustomerInvoice {
+    customerInvoice.subtotal = subtotal;
+    customerInvoice.vatPercentage = 0.21;
+    customerInvoice.vat = customerInvoice.vatPercentage * customerInvoice.subtotal;
+    customerInvoice.total = customerInvoice.subtotal + customerInvoice.vat;
+
+
     return customerInvoice;
 
   }
@@ -147,7 +149,7 @@ export class AddCustomerInvoiceComponent implements OnInit {
 
 
 
-  }
+}
 
 
 
