6cd2d1ee08f7dbca65b73b286ae62ccd4ba395c4
jesustt <minicascos@gmail.com>
2020-11-20 18:25:30 +0100

    Customer,Provider and Product Edit and Delete  earlyaccess

--- src/app/components/invoices/invoices.component.ts
+++ src/app/components/invoices/invoices.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomerInvoice } from 'src/app/models/customerInvoice';
+import { ProviderInvoice } from 'src/app/models/providerInvoice';
+import { InvoiceService } from 'src/app/services/invoice.service';
 
 @Component({
   selector: 'app-invoices',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoices.component.css']
 })
 export class InvoicesComponent implements OnInit {
+  selectedCustomerInvoice: CustomerInvoice;
+  selectedProviderInvoice: ProviderInvoice;
 
-  constructor() { }
+
+  titulo: string=" Invoice List";
+  customerInvoiceList:CustomerInvoice[];
+  providerInvoiceList:ProviderInvoice[];
+
+
+  constructor(private service:InvoiceService, private router:Router) { }
 
   ngOnInit(): void {
+    this.service.findAllCustomerInvoice().subscribe(customerInvoices =>{
+      this.customerInvoiceList=customerInvoices;
+    });
+
+    this.service.findAllProviderInvoice().subscribe(providerInvoices =>{
+      this.providerInvoiceList=providerInvoices;
+    });
+  }
+
+
+  onSelectProvider(providerInvoice):void{
+    this.selectedProviderInvoice=providerInvoice;
+  }
+
+
+
+  onSelectCustomer(customerInvoice):void{
+    this.selectedCustomerInvoice=customerInvoice;
   }
 
 }
