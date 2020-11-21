9d64c8cff0243d5c3607da15c5577b5ef31a86d1
jesustt <minicascos@gmail.com>
2020-11-21 04:45:11 +0100

    Add Invoice and See Invoice

--- src/app/components/individual-customer-invoice/individual-customer-invoice.component.ts
+++ src/app/components/individual-customer-invoice/individual-customer-invoice.component.ts
@@ -41,34 +41,42 @@ export class IndividualCustomerInvoiceComponent implements OnInit {
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
       console.log("TRAMO 2 ID DE LA FACTURA:" + this.id);
-     
+
+      this.serviceInvoice.findCustomerInvoiceById(+this.id).subscribe(customerInvoice => {
+        this.customerInvoice = customerInvoice;
+        this.newCustomerInvoice = customerInvoice;
+        console.log("TRAMO 3");
+        console.log("Customer" + this.newCustomerInvoice.customerId);
+        console.log("If" + this.newCustomerInvoice.id);
+        console.log("Fecha" + this.newCustomerInvoice.invoiceDate);
+
+
+
+
+        console.log("TRAMO 3,5");
+        console.log("Detalle" + 0 + "Id del producto" + this.newCustomerInvoice.details[0].productId);
+        for (let i = 0; i < this.newCustomerInvoice.details.length; i++) {
+          this.idProductList.push(this.newCustomerInvoice.details[i].productId);
+          console.log("Detalle" + i + "Id del producto" + this.idProductList[i]);
+        }
+
+
+        console.log("TRAMO 4");
+        this.serviceProduct.findAllProductInIds(this.idProductList).subscribe(productDetailList => {
+          this.productDetailList = productDetailList;
+
+
+        });
+
+        this.detailList = this.newCustomerInvoice.details;
+        console.log("TRAMO 5");
+      });
     });
 
-    this.serviceInvoice.findCustomerInvoiceById(+this.id).subscribe(customerInvoice => {
-      this.customerInvoice = customerInvoice;
-      this.newCustomerInvoice = customerInvoice;
-      console.log("TRAMO 3");
-      console.log("Customer" + this.newCustomerInvoice.customerId);
-      console.log("If" + this.newCustomerInvoice.id);
-      console.log("Fecha" + this.newCustomerInvoice.invoiceDate);
-    });
-
-    console.log("TRAMO 3,5");
-  console.log("Detalle" + 0 + "Id del producto" + this.newCustomerInvoice.details[0].productId);
-    for (let i = 0; i < this.newCustomerInvoice.details.length; i++) {
-      this.idProductList.push(this.newCustomerInvoice.details[i].productId);
-      console.log("Detalle" + i + "Id del producto" + this.idProductList[i]);
-    }
-
-    console.log("TRAMO 4");
-    this.serviceProduct.findAllProductInIds(this.idProductList).subscribe(productDetailList => {
-      this.productDetailList = productDetailList;
 
 
-    });
 
-    this.detailList = this.newCustomerInvoice.details;
-    console.log("TRAMO 5");
+
   }
 
 
